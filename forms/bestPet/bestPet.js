626f55f4d457071f3b6445cd3da0016181ae5569
brennamaloy <bkm91466@creighton.edu>
2020-09-30 21:31:40 -0500

    Initial commit

---
+++ forms/bestPet/bestPet.js
@@ -0,0 +1,3 @@
+let bestPetDog = "Dogs are likely the most stereotypical pet for children–and there’s a reason for that. \r\n  Most average-sized dogs are large enough to avoid being injured by your child, but many breeds are also calm and laidback with children. Dogs easily live to a decade, \r\n so you shouldn’t have to worry about having that uncomfortable conversation with your child. \r\n This species is also trainable and can usually go anywhere your child can, so they make good everyday companions."
+let replacedHamster = bestPetDog.replace("Dogs", "Hamsters")
+alert(replacedHamster)
